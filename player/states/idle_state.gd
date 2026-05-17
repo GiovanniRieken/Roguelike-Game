8fa3132b8cc6e518bd418bb8f0b6cc0e4d48e300
Snelleuh <r.snelleuh@gmail.com>
2026-05-17 21:27:49 +0200

    Updated IdleState.gd

--- player/states/idle_state.gd
+++ player/states/idle_state.gd
@@ -1,3 +1,4 @@
+class_name IdleState
 extends LimboState
 
 var player: Player
@@ -10,6 +11,6 @@ func _setup() -> void:
 func _update(delta: float) -> void:
 	player.velocity.x = move_toward(player.velocity.x, 0, player.friction * delta)
 	player.velocity.z = move_toward(player.velocity.z, 0, player.friction * delta)
-	
+
 	if player.input_direction != Vector2.ZERO:
 		dispatch(EVENT_FINISHED)
