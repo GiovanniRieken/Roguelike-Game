03d4e5a3c2b750bbde1275c7aa18947f942f4b9e
Giovanni Rieken <giovannirieken2010@gmail.com>
2026-05-21 13:47:31 +0200

    Revert "Updated move_state.gd"
    This reverts commit 3d2ca2ec855b149706b4ff2fb8e0b132fa919ff9.

--- player/states/move_state.gd
+++ player/states/move_state.gd
@@ -1,4 +1,3 @@
-class_name MoveState
 extends LimboState
 
 var player: Player
@@ -9,13 +8,12 @@ func _setup() -> void:
 
 
 func _update(delta: float) -> void:
-	if player.input_direction == Vector2.ZERO:
-		dispatch(EVENT_FINISHED)
-		return
-
 	var input_direction := Vector3(player.input_direction.x, 0, player.input_direction.y)
 	var direction := (player.transform.basis * input_direction).normalized()
 	var target_velocity := direction * player.speed
 
 	player.velocity.x = move_toward(player.velocity.x, target_velocity.x, player.acceleration * delta)
 	player.velocity.z = move_toward(player.velocity.z, target_velocity.z, player.acceleration * delta)
+
+	if player.input_direction == Vector2.ZERO:
+		dispatch(EVENT_FINISHED)
